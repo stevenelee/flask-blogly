25220df58f9bfcacfad47cbcab4be02cd8254360
Steven Lee <stevelee83@gmail.com>
2023-04-26 13:15:51 -0700

    make minor changes to code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, User
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -14,3 +14,4 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
+# @app.routes
