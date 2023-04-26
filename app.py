c9a5d36f0d59111242bd439f3bc1dbff7b24853c
Steven Lee <stevelee83@gmail.com>
2023-04-26 12:32:39 -0700

    create templates

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +13,4 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(
+       db.Integer,
+       primary_key=True,
+       autoincrement=True)
+
+    first_name = db.Column(
+       db.String(50),
+       nullable=False)
+
+    last_name = db.Column(
+       db.String(50),
+       nullable=False)
+
+    image_url = db.Column(
+        db.String(255),
+        nullable=False,
+        default="https://i.pinimg.com/550x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg"
+    )
